c6dfef40980b14463e4b538c20aeb0ebe30a7f3a
almasabdykadyr <almasabdykadir@gmail.com>
2024-10-21 05:08:24 +0500

    add author

---
+++ src/main/java/dev/almasabdykadyr/library/dto/AuthorRequest.java
@@ -0,0 +1,9 @@
+package dev.almasabdykadyr.library.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+/**
+ * DTO for {@link dev.almasabdykadyr.library.entity.Author}
+ */
+public record AuthorRequest(@NotBlank String firstName, @NotBlank String lastName) {
+}
