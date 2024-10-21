7c261c418b0d0c9f4d48f83dea9dfd3ca7b9b581
almasabdykadyr <almasabdykadir@gmail.com>
2024-10-21 23:06:42 +0500

    add controller exception handler

---
+++ src/main/java/dev/almasabdykadyr/library/aop/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package dev.almasabdykadyr.library.aop;
+
+import dev.almasabdykadyr.library.exception.BookNotFoundException;
+import dev.almasabdykadyr.library.exception.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFound(UserNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(BookNotFoundException.class)
+    public ResponseEntity<String> handleBookNotFound(BookNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleBadRequest(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+}
