c0cee21e27c11b0d9932e344a632e3ce7afa6ad0
almasabdykadyr <almasabdykadir@gmail.com>
2024-10-25 22:56:21 +0500

    add transactions

---
+++ src/main/java/dev/almasabdykadyr/library/config/AppConfig.java
@@ -0,0 +1,11 @@
+package dev.almasabdykadyr.library.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableScheduling
+@EnableTransactionManagement]
+@Configuration
+public class AppConfig {
+}
