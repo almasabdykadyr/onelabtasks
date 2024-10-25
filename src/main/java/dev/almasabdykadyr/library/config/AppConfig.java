2e819afa347d454d297b4dc22b40da03f92a6c3e
almasabdykadyr <almasabdykadir@gmail.com>
2024-10-25 22:56:40 +0500

    fix conf

--- src/main/java/dev/almasabdykadyr/library/config/AppConfig.java
+++ src/main/java/dev/almasabdykadyr/library/config/AppConfig.java
@@ -5,7 +5,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableScheduling
-@EnableTransactionManagement]
+@EnableTransactionManagement
 @Configuration
 public class AppConfig {
 }
