401390340cf0468b86ac80f4836f8cba161fa77d
almasabdykadyr <almasabdykadir@gmail.com>
2024-11-04 00:02:46 +0500

    swagger hotfix

--- src/main/java/dev/almasabdykadyr/library/controller/AuthApi.java
+++ src/main/java/dev/almasabdykadyr/library/controller/AuthApi.java
@@ -3,17 +3,36 @@ package dev.almasabdykadyr.library.controller;
 import dev.almasabdykadyr.library.dto.AuthRequest;
 import dev.almasabdykadyr.library.dto.AuthResponse;
 import dev.almasabdykadyr.library.dto.RegisterRequest;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import io.swagger.v3.oas.annotations.Operation;
 
 @RequestMapping("api/v1/auth")
+@Tag(name = "Auth API", description = "API for Rental Service")
 public interface AuthApi {
 
+    @Operation(
+            summary = "Register a new user",
+            description = "Endpoint for registering a new user",
+            responses = {
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Registration successful"),
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "Invalid request")
+            }
+    )
     @PostMapping(value = "/register", consumes = "application/json", produces = "application/json")
     ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request);
 
+    @Operation(
+            summary = "Authenticate user",
+            description = "Endpoint for user authentication",
+            responses = {
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Authentication successful"),
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "401", description = "Unauthorized")
+            }
+    )
     @PostMapping(value = "/authenticate", consumes = "application/json", produces = "application/json")
     ResponseEntity<AuthResponse> authenticate(@RequestBody AuthRequest request);
 }
--- src/main/java/dev/almasabdykadyr/library/controller/BookApi.java
+++ src/main/java/dev/almasabdykadyr/library/controller/BookApi.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @RequestMapping("api/v1/")
+@Tag(name = "Book API", description = "API for Book Service")
 public interface BookApi {
 
     @Operation(
