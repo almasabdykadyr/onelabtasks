e471d1bfb0767f38f848b8169e74d12f59619ff3
almasabdykadyr <almasabdykadir@gmail.com>
2024-11-03 20:13:19 +0500

    refactor security

--- src/main/java/dev/almasabdykadyr/library/controller/AuthController.java
+++ src/main/java/dev/almasabdykadyr/library/controller/AuthController.java
@@ -6,30 +6,21 @@ import dev.almasabdykadyr.library.dto.RegisterRequest;
 import dev.almasabdykadyr.library.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/auth")
 @RequiredArgsConstructor
-public class AuthController {
+public class AuthController implements AuthApi {
 
     private final AuthService authService;
 
-    @PostMapping("/register")
-    public ResponseEntity<AuthResponse> register(
-            @RequestBody RegisterRequest request
-    ) {
+    @Override
+    public ResponseEntity<AuthResponse> register(RegisterRequest request) {
         return ResponseEntity.ok(authService.register(request));
     }
 
-    @PostMapping("/authenticate")
-    public ResponseEntity<AuthResponse> authenticate(
-            @RequestBody AuthRequest request
-    ) {
+    @Override
+    public ResponseEntity<AuthResponse> authenticate(AuthRequest request) {
         return ResponseEntity.ok(authService.authenticate(request));
     }
-
 }
